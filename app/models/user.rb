5636419a0d1b54ad04d3b2e97f3ad08327ace25d
Jason L Perry <jasper@ambethia.com>
2015-04-25 16:44:39 -0400

    Restrict login to admins of the current cohort

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 
   def self.authorize_with(auth)
+    return nil unless Cohort.current.admins_include?(auth.info.nickname)
     user = where(provider: auth["provider"], uid: auth["uid"]).first_or_create
     user.tap do |u|
       u.name         = auth.info.name
