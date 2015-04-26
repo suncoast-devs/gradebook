c5d0efcaec465f07e4d19931ed3e310257c7763f
Jason L Perry <jasper@ambethia.com>
2015-04-25 22:27:59 -0400

    Update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery :with => :exception
+  force_ssl :if => :ssl_configured?
+
   helper_method :current_user
 
   private
@@ -14,5 +16,9 @@ class ApplicationController < ActionController::Base
     unless current_user
       redirect_to root_url, :alert => "Authentication Required."
     end
+  end
+
+  def ssl_configured?
+    Rails.env.production?
   end
 end
