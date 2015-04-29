335042ada668805a1e7f5f26b6a23c6eb942c1f0
Jason L Perry <jasper@ambethia.com>
2015-04-29 01:37:21 -0400

    Seed and issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  API_ACTIONS = [:index, :show, :create, :update, :destroy]
   root 'home#index'
 
   get "/auth/:provider/callback" => "sessions#create"
   get "/signout" => "sessions#destroy", :as => :signout
 
   resources :cohorts
-  resources :homework, :only => API_ACTIONS
-  resources :students, :only => API_ACTIONS do
-    resources :assignments, :only => API_ACTIONS
+  resources :homework, :only => [:index, :show, :create, :update, :destroy]
+  resources :students, :only => [:index, :show, :create, :update, :destroy] do
+    get :issues, :on => :member
+    resources :assignments, :only => [:index, :show, :create, :update, :destroy]
   end
 end
