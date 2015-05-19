318f186b561e3a1a34b8735b00b22961f142d056
Jason L Perry <jasper@ambethia.com>
2015-05-19 14:56:15 -0400

    Authed requests

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -5,6 +5,11 @@ class Cohort < ActiveRecord::Base
   scope :current, -> { first } # TODO: Make this a flag
 
   def admins_include?(nickname)
-    Octokit.org_members(org, :role => 'admin').map(&:login).include?(nickname)
+    client if user = User.first
+      Octokit::Client.new(:access_token => user.access_token)
+    else
+      Octokit
+    end
+    client.org_members(org, :role => 'admin').map(&:login).include?(nickname)
   end
 end
