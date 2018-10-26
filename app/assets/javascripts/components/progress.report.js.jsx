8c4b908a2fd75cb23be19c04f186f61a024e7fc2
Mark Dewey <mark.t.dewey@gmail.com>
2018-10-26 12:03:43 -0400

    ready to pivot

--- app/assets/javascripts/components/progress.report.js.jsx
+++ app/assets/javascripts/components/progress.report.js.jsx
@@ -5,39 +5,55 @@ var BS = ReactBootstrap;
 
 var StudentProgressReport = React.createClass({
 
-    getInitialState: function () {
-      return {
-        students: [],
-        homework: []
-      };
-    },
-  
-    fetchData: function () {
-      fetch(`/students/${this.props.student.id}/assignments.json`)
-        .then(resp => resp.json())
-        .then(json => {
-          console.log(json)
-          this.setState({
-            allAssignments:json
-          })
+  getInitialState: function () {
+    return {
+      homework: []
+    };
+  },
+
+  fetchData: function () {
+    fetch(`/students/${this.props.student.id}/assignments.json`)
+      .then(resp => resp.json())
+      .then(json => {
+        console.log(json)
+        this.setState({
+          homework: json
         })
-    },
-  
-    handleCreate: function (data) {
-     
-    },
-  
-    componentDidMount: function () {
-      this.fetchData();
-    },
-  
-    render: function () {
-      console.log({props:this.props})
-      return <div>
-            <section className="student-profile">
-                <header className="student-name">{this.props.student.name}</header>
-                <header className="cohort-name">{this.props.cohort.name}</header>
-            </section>
-      </div>;
-    }
-  });
+      })
+  },
+
+  componentDidMount: function () {
+    this.fetchData();
+  },
+
+  render: function () {
+    console.log({ props: this.props })
+    return <div>
+      <section className="student-profile">
+        <header className="student-name">{this.props.student.name}</header>
+        <header className="cohort-name">{this.props.cohort.name}</header>
+      </section>
+      <section>
+        <header>Select Assignments</header>
+        <section>
+          <ul>
+            {this.state.homework.map((hw) => {
+              return (<li key={hw.id}>
+                <input type="checkbox" id={hw.id} />
+                <label htmlFor={hw.id}>
+                  <span>
+                    {hw.homework.name}
+                  </span>
+                  |
+                      <span>
+                    {hw.score}
+                  </span>
+                </label>
+              </li>)
+            })}
+          </ul>
+        </section>
+      </section>
+    </div>;
+  }
+});
