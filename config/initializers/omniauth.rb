9ee26a3004d51b176810569ba3b2c91153470604
Mark Dewey <mark.t.dewey@gmail.com>
2018-11-01 13:47:15 -0400

    fixed for PR

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github,
-  "e3d08edc5b52e5ab8f31",
-    "0c8c24d2c19739c25bd708d7ebf77900cb6dd421",
+  ENV['GITHUB_KEY'],
+  ENV['GITHUB_SECRET'],
     scope: "user:email,public_repo"
 end
